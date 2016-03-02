1f2bb65b7dfb6f035877a48575e7b6fc0675a34d
Ian Coolidge <ian@boundarydevices.com>
2016-03-02 09:48:28 -0800

    Remove ft5x06 related calibrators. Handled by kernel now

--- recipes-graphics/xinput-calibrator/files/calibrate-ft5x06.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xDIMENS=`DISPLAY=:0 xdpyinfo | grep dimensions | awk '{print $2}' | awk -Fx '{print $1}'`
-yDIMENS=`DISPLAY=:0 xdpyinfo | grep dimensions | awk '{print $2}' | awk -Fx '{print $2}'`
-
-DISPLAY=:0 xinput set-int-prop "ft5x06" "Evdev Axis Calibration" 32 0 $xDIMENS 0 $yDIMENS
