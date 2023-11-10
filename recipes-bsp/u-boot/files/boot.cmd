9f68ad0d1e2efb4800561ceb632cf7ff4b413b2a
Gary Bisson <gary.bisson@boundarydevices.com>
2023-11-10 10:29:58 +0100

    u-boot-script-tungsten-700: introduce cmd_custom variable
    - to be similar to our imx bootscripts
    - allows adding/overwriting bootargs values if needed
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- recipes-bsp/u-boot/files/boot.cmd
+++ recipes-bsp/u-boot/files/boot.cmd
@@ -8,14 +8,18 @@ if itest.s "x" == "x${distro_bootpart}" ; then
 	part number ${devtype} ${devnum} rootfs distro_bootpart
 fi
 
+setenv bootargs ${bootargs} console=ttyS0,115200;
+
+if itest.s "x" != "x${cmd_custom}" ; then
+	run cmd_custom
+fi
+
 setexpr bpart fmt %d ${distro_bootpart}
 if test "usb" = "${devtype}" ; then
 	setenv bootargs "${bootargs} root=/dev/sda${bpart}";
 else
 	setenv bootargs "${bootargs} root=/dev/mmcblk${devnum}p${bpart}";
 fi
-
-setenv bootargs ${bootargs} console=ttyS0,115200;
 
 read ${devtype} ${devnum}#kernel ${loadaddr} 0 0xffff;
 bootm ${loadaddr}#conf-mediatek_mt8390-tungsten_smarc.dtb#conf-apusys.dtbo#conf-${display_dtbo}#conf-gpu-mali.dtbo#conf-video.dtbo;
