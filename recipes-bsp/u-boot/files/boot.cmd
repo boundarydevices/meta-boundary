7b7c5e0464a6017d9d4eae096ddaabe8951d3e00
Chris Dimich <chris.dimich@boundarydevices.com>
2023-11-09 23:00:01 -0800

    u-boot-script-tungsten-700: remove hardcoded variables
    Signed-off-by: Chris Dimich <chris.dimich@boundarydevices.com>

--- recipes-bsp/u-boot/files/boot.cmd
+++ recipes-bsp/u-boot/files/boot.cmd
@@ -4,9 +4,14 @@ if itest.s x${display_dtbo} == x ; then
 	setenv display_dtbo display-hdmi.dtbo;
 fi
 
-if itest.s "x" == "x${bpart}" ; then
-	bpart=10
+if itest.s "x" == "x${distro_bootpart}" ; then
+	part number ${devtype} ${devnum} rootfs distro_bootpart
 fi
+
+setexpr b0 ${distro_bootpart} % 0x0a;
+setexpr b1 ${distro_bootpart} / 0x0a;
+setexpr bpart ${b1} * 0x10
+setexpr bpart ${bpart} + ${b0};
 
 if test "usb" = "${devtype}" ; then
 	setenv bootargs "${bootargs} root=/dev/sda${bpart}";
@@ -16,5 +21,7 @@ fi
 
 setenv bootargs ${bootargs} console=ttyS0,115200;
 
-read mmc 0:9 $loadaddr 0 0xffff;
-bootm $loadaddr#conf-mediatek_mt8390-tungsten_smarc.dtb#conf-apusys.dtbo#conf-${display_dtbo}#conf-gpu-mali.dtbo#conf-video.dtbo;
+part number ${devtype} ${devnum} kernel kernelpart
+
+read ${devtype} ${devnum}:${kernelpart} ${loadaddr} 0 0xffff;
+bootm ${loadaddr}#conf-mediatek_mt8390-tungsten_smarc.dtb#conf-apusys.dtbo#conf-${display_dtbo}#conf-gpu-mali.dtbo#conf-video.dtbo;
